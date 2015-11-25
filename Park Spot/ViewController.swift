7bdd6800469a8376a4e71c2948ad160827d5ede6
Bobby Rehm <bobbyrehm502@gmail.com>
2015-11-25 14:37:08 -0500

    Commit before merge

--- Park Spot/ViewController.swift
+++ Park Spot/ViewController.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
+let SPOT_KEY = "PARKING_SPOT_KEY"
+
 class ViewController: UIViewController {
+    
+    let spots: [String] = ["Stairway 2", "Stairway 3", "Parking Garage"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let alertController = UIAlertController(title: "Park Spot", message: "Previous Spot: \(getPreviousSpot())", preferredStyle: UIAlertControllerStyle.Alert)
+        for spot in spots {
+            let action = UIAlertAction(title: spot, style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction) -> Void in
+                self.setSpot(spot)
+            })
+            alertController.addAction(action)
+        }
+        let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (action: UIAlertAction) -> Void in
+            abort()
+        }
+        alertController.addAction(closeAction)
+        self.presentViewController(alertController, animated: true, completion: nil);
+    }
+    
+    func setSpot(spot: String) {
+        NSUserDefaults.standardUserDefaults().setValue(spot, forKey: SPOT_KEY)
+        abort()
+    }
+    
+    func getPreviousSpot() -> String {
+        if let previousSpot = NSUserDefaults.standardUserDefaults().valueForKey(SPOT_KEY) as? String {
+            return previousSpot
+        } else {
+            return "None"
+        }
     }
 
     override func didReceiveMemoryWarning() {
